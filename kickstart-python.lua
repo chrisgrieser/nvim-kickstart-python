d5616e245798ccd0dba2102533a6121a2c82e999
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-07-20 11:51:44 +0200

    fix: false dap-python diagnostics when using lazydev

--- kickstart-python.lua
+++ kickstart-python.lua
@@ -375,7 +375,7 @@ local plugins = {
 			-- uses the debugypy installation by mason
 			local debugpyPythonPath = require("mason-registry").get_package("debugpy"):get_install_path()
 				.. "/venv/bin/python3"
-			require("dap-python").setup(debugpyPythonPath, {})
+			require("dap-python").setup(debugpyPythonPath, {}) ---@diagnostic disable-line: missing-fields
 		end,
 	},
 
