b79233ae46ffc3ba79d85a0b75c7bd67c13da7c4
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-07-21 14:05:46 +0200

    chore: move python-ft-config to the bottom

--- kickstart-python.lua
+++ kickstart-python.lua
@@ -17,42 +17,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 --------------------------------------------------------------------------------
--- BASIC PYTHON-RELATED OPTIONS
-
--- The filetype-autocmd runs a function when opening a file with the filetype
--- "python". This method allows you to make filetype-specific configurations. In
--- there, you have to use `opt_local` instead of `opt` to limit the changes to
--- just that buffer. (As an alternative to using an autocmd, you can also put those
--- configurations into a file `/after/ftplugin/{filetype}.lua` in your
--- nvim-directory.)
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "python", -- filetype for which to run the autocmd
-	callback = function()
-		-- use pep8 standards
-		vim.opt_local.expandtab = true
-		vim.opt_local.shiftwidth = 4
-		vim.opt_local.tabstop = 4
-		vim.opt_local.softtabstop = 4
-
-		-- folds based on indentation https://neovim.io/doc/user/fold.html#fold-indent
-		-- if you are a heavy user of folds, consider using `nvim-ufo`
-		vim.opt_local.foldmethod = "indent"
-
-		local iabbrev = function(lhs, rhs) vim.keymap.set("ia", lhs, rhs, { buffer = true }) end
-		-- automatically capitalize boolean values. Useful if you come from a
-		-- different language, and lowercase them out of habit.
-		iabbrev("true", "True")
-		iabbrev("false", "False")
-
-		-- in the same way, we can fix habits regarding comments or None
-		iabbrev("--", "#")
-		iabbrev("null", "None")
-		iabbrev("none", "None")
-		iabbrev("nil", "None")
-	end,
-})
-
---------------------------------------------------------------------------------
 
 local plugins = {
 	-- TOOLING: COMPLETION, DIAGNOSTICS, FORMATTING
@@ -379,3 +343,39 @@ local plugins = {
 
 -- tell lazy.nvim to load and configure all the plugins
 require("lazy").setup(plugins)
+
+--------------------------------------------------------------------------------
+-- SETUP BASIC PYTHON-RELATED OPTIONS
+
+-- The filetype-autocmd runs a function when opening a file with the filetype
+-- "python". This method allows you to make filetype-specific configurations. In
+-- there, you have to use `opt_local` instead of `opt` to limit the changes to
+-- just that buffer. (As an alternative to using an autocmd, you can also put those
+-- configurations into a file `/after/ftplugin/{filetype}.lua` in your
+-- nvim-directory.)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "python", -- filetype for which to run the autocmd
+	callback = function()
+		-- use pep8 standards
+		vim.opt_local.expandtab = true
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.tabstop = 4
+		vim.opt_local.softtabstop = 4
+
+		-- folds based on indentation https://neovim.io/doc/user/fold.html#fold-indent
+		-- if you are a heavy user of folds, consider using `nvim-ufo`
+		vim.opt_local.foldmethod = "indent"
+
+		local iabbrev = function(lhs, rhs) vim.keymap.set("ia", lhs, rhs, { buffer = true }) end
+		-- automatically capitalize boolean values. Useful if you come from a
+		-- different language, and lowercase them out of habit.
+		iabbrev("true", "True")
+		iabbrev("false", "False")
+
+		-- in the same way, we can fix habits regarding comments or None
+		iabbrev("--", "#")
+		iabbrev("null", "None")
+		iabbrev("none", "None")
+		iabbrev("nil", "None")
+	end,
+})
