d6865d34d9670f223e33576f2cabab1eab5f783a
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-07-21 14:02:28 +0200

    improv: proper lazy bootstrap for 0.10

--- kickstart-python.lua
+++ kickstart-python.lua
@@ -1,11 +1,20 @@
--- BOOTSTRAP the plugin manager `lazy.nvim`
+-- BOOTSTRAP the plugin manager `lazy.nvim` https://lazy.folke.io/installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
+local lazyLocallyAvailable = vim.uv.fs_stat(lazypath) ~= nil
+if not lazyLocallyAvailable then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then error("Error cloning lazy.nvim:\n" .. out) end
+	local out = vim.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }):wait()
+	if out.code ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 --------------------------------------------------------------------------------
 -- BASIC PYTHON-RELATED OPTIONS
